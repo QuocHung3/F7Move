8bd651ef054f44887abc8b50da3c5291260bb9ba
QuocHung3 <nongquochung0607@gmail.com>
2023-09-21 13:41:48 +0700

    cau hinh du an - code giao dien gioi thieu

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+import Splash from "./components/splash/Splash";
+import OnBoarding1 from "./components/onBoarding1/OnBoarding1";
+import OnBoarding2 from "./components/onBoarding2/OnBoarding2";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  const forFade = ({ current }) => ({
+    cardStyle: {
+      opacity: 0,
+    },
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyleInterpolator: forFade,
+        }}
+      >
+        <Stack.Screen
+          name="Splash"
+          component={Splash}
+          options={{ cardStyleInterpolator: forFade }}
+        />
+        <Stack.Screen name="OnBoarding1" component={OnBoarding1} />
+        <Stack.Screen name="OnBoarding2" component={OnBoarding2} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
